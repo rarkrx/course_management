048b31ba6df95420582d57d3d784768f4d3ac8b3
rarkrx <frostrarkrx@gmail.com>
2024-10-03 15:55:42 +0800

    add table functionality

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,23 @@
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 import shutil
 import os
+import uvicorn
+import pandas as pd
 
 app = FastAPI()
+origins = [
+    "http://localhost:3001",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Directory where files will be saved
 UPLOAD_DIRECTORY = "./uploads/"
@@ -17,5 +31,47 @@ async def upload_file(file: UploadFile = File(...)):
     
     with open(file_location, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
+        
+    # Read the Excel file into a pandas DataFrame
+    df = pd.read_excel(file_location)
+    df = df.fillna('')
     
-    return JSONResponse(content={"message": "File uploaded successfully", "filename": file.filename})
+    columns = df.columns.tolist()
+    #print(columns)
+    # Step 4: Dynamically create the SQLite table based on the Excel columns
+    # Form a SQL CREATE TABLE statement dynamically
+    columns_list = []
+    for column in columns:
+        column = column.replace(" ", "_").upper()
+        if column.upper().find('DATE')>-1:
+            columns_list.append(f"'{column}' DATE")
+            
+        else:
+            columns_list.append(f"'{column}' TEXT")
+            
+    df.columns = columns_list 
+    
+    api_columns = []
+    column_index=0
+    for column_name in df.columns.values.tolist():
+        api_columns.append(
+            {
+                "Header": column_name,
+                "accessor": f"{column_index}"
+            }
+        )
+        column_index += 1
+    
+    # Convert the DataFrame to JSON
+    df_split = df.to_dict(orient="split")
+    data_json = df_split['data']
+    data_json.insert(0,columns_list)
+    print(data_json)
+    return JSONResponse(content={"message": "File uploaded successfully", 
+                                 "filename": file.filename,
+                                 "data" : data_json,
+                                 "columns" : api_columns,
+                                 })
+
+if __name__ == '__main__':
+    uvicorn.run("app:app", port=5000, reload=True, access_log=False)
--- course_management_frontend/src/views/FileUpload.js
+++ course_management_frontend/src/views/FileUpload.js
@@ -1,9 +1,13 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import Table from '../table/Table'
+
 
 const FileUpload = () => {
   const [file, setFile] = useState(null);
   const [message, setMessage] = useState('');
+  const [tableData, setTableData] = useState([]);
+  const [columns, setColumns] = useState([]);
 
   const handleFileChange = (e) => {
     setFile(e.target.files[0]);
@@ -16,11 +20,15 @@ const FileUpload = () => {
     formData.append('file', file);
 
     try {
-      const response = await axios.post('http://127.0.0.1:8000/upload', formData, {
+      const response = await axios.post('http://127.0.0.1:5000/upload', formData, {
         headers: {
           'Content-Type': 'multipart/form-data',
         },
       });
+      console.log(response)
+      // Set table columns and data
+      setColumns(response.data.columns);
+      setTableData(response.data.data);
       setMessage(response.data.message);
     } catch (error) {
       console.error('Error uploading file:', error);
@@ -35,6 +43,10 @@ const FileUpload = () => {
         <button type="submit">Upload File</button>
       </form>
       <p>{message}</p>
+      {/* Display the table if data is available */}
+      {tableData.length > 0 && (
+        <Table columns={columns} data={tableData} completeData={tableData} query="excel_data" />
+      )}
     </div>
   );
 };
